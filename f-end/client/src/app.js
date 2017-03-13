14727877868f95d466239117073f2114f0b02b34
Sean Ho <seansinflipflops@gmail.com>
2017-03-13 14:53:51 -0400

    feat(rendering current rates after clicking button):
    click button to render current interest rate data.

--- f-end/client/src/app.js
+++ f-end/client/src/app.js
@@ -1,13 +1,69 @@
-angular.module('Widget', [])
+angular.module('Widget', ['chart.js'])
 
 .factory('CurrentRates',($http) => {
 
-  const rateSearch() => {
+  const rateSearch = () => {
     return $http({
       method: `GET`,
-      url: `/api/currentrates`
+      url: `/api/currentrates`,
+      headers: `text/html`
     })
     .then(response => response);
   }
 
+  return {
+    rateSearch: rateSearch
+  }
+
+})
+
+//current loan rates
+.controller("CurrentRate", function($scope, CurrentRates) {
+  $scope.rates = {};
+  $scope.currentRate = () => {
+    CurrentRates.rateSearch()
+    .then((response) => {
+      let data = response.data;
+      $scope.rates.directSubsidizedUndergrad = data.directSubsidizedUndergrad;
+      $scope.rates.directUnsubsidizedUndergrad = data.directUnsubsidizedUndergrad;
+      $scope.rates.directUnsubsidizedGrad = data.directUnsubsidizedGrad;
+      $scope.rates.directPlus = data.directPlus;
+    })
+  }
+})
+
+.controller("InputController", function($scope) {
+
+})
+
+.controller("LineCtrl", function ($scope) {
+
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  $scope.series = ['Scholarship A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
+  $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2' }];
+  $scope.options = {
+    scales: {
+      yAxes: [
+        {
+          id: 'y-axis-1',
+          type: 'linear',
+          display: true,
+          position: 'left'
+        },
+        {
+          id: 'y-axis-2',
+          type: 'linear',
+          display: true,
+          position: 'right'
+        }
+      ]
+    }
+  };
 })
--- f-end/server/server.js
+++ f-end/server/server.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/../src/')));
+app.use(express.static(path.join(__dirname, '/../client/')));
 app.use(express.static(path.join(__dirname, '/../node_modules')));
 app.use(bodyParser.json());
+
+//api call for the current rates
 
 app.get('/api/currentrates', (req, res) => {
   const body = {
     directSubsidizedUndergrad: `3.76%`,
-    directUnsubsidizedUndergrade: `3.76%`,
+    directUnsubsidizedUndergrad: `3.76%`,
     directUnsubsidizedGrad: `5.31%`,
     directPlus: `6.31%`
   }
-  res.send(body);
-})
+  res.json(body);
+});
+
+
+//set port
+const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
   if(err) {
     console.log('Error occured : ', err);
   }
   console.log('Server is listening to port : ', port);
-})
+});
