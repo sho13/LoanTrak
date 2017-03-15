9165cda1676059cee5725870bf435e70d83c1c1d
Sean Ho <seansinflipflops@gmail.com>
2017-03-15 02:27:25 -0400

    feat:
    feature that allows user to choose if they want to pay something other than minimal payment

--- f-end/client/src/app.js
+++ f-end/client/src/app.js
@@ -9,43 +9,49 @@ angular.module('Widget', ['chart.js', 'ng', 'ngMaterial', 'ngAnimate', 'ngAria']
 
   let options = {};
 
-  const inputData = () => {
-    return $http.post(`/api/loaninfo`, options)
-    .then((response) => {
-      if(Object.keys(response.data).length === 0) {
+  const inputData = (options) => {
+    return new Promise((resolve, reject) => {
+      if(Object.keys(options).length === 0) {
         alert("Please fill in all information!")
         return
-      } else if(Object.keys(response.data.scholarship).length === 0 || Object.keys(response.data.interest).length === 0 || Object.keys(response.data.loanPeriod).length === 0) {
+      } else if(Object.keys(options.scholarship).length === 0 || Object.keys(options.interest).length === 0 || Object.keys(options.loanPeriod).length === 0) {
         alert("Please fill in all information!")
         return
       }
-
       let data = {
-        scholarship: parseInt(response.data.scholarship),
-        interest: parseFloat(response.data.interest),
-        loanPeriod: parseInt(response.data.loanPeriod)
+        scholarship: parseInt(options.scholarship),
+        interest: parseFloat(options.interest),
+        loanPeriod: parseInt(options.loanPeriod),
+        optionalPayment: options.optionalPayment ? parseFloat(options.optionalPayment) : null
       }
-
+      console.log(data);
       let results = {
         scholarship: data.scholarship,
         monthlyPayment: parseFloat(((data.scholarship * data.interest) / data.loanPeriod).toFixed(2)),
+        optionalPayment: data.optionalPayment,
         months: data.loanPeriod,
         labels: [],
         payments: [data.scholarship],
-        series: ['Scholarship']
+        alternativePayments: [data.scholarship],
+        series: ['Minimal Amount', 'Your Payment Choice']
       }
 
       let scholarship = results.scholarship;
+      let scholarship2 = results.scholarship;
 
       for(let i = 0; i < results.months; i++) {
         results.labels.push(i.toString());
         scholarship -= results.monthlyPayment;
         if(scholarship >= 0) {
-            results.payments.push(parseFloat(scholarship.toFixed(2)))
+          results.payments.push(parseFloat(scholarship.toFixed(2)))
+          if(results.optionalPayment !== null) {
+            scholarship2 -= results.optionalPayment;
+            results.alternativePayments.push(parseFloat(scholarship2.toFixed(2)))
+          }
         }
       }
 
-      return results
+      resolve(results)
     });
   }
 
@@ -101,17 +107,14 @@ angular.module('Widget', ['chart.js', 'ng', 'ngMaterial', 'ngAnimate', 'ngAria']
 
   $scope.input = function () {
     Services.inputData($scope.query)
-    .then((response) => {
-      return response
-    })
     .then((data) => {
       $scope.monthly = data.monthlyPayment;
       $scope.months = Services.options.loanPeriod;
       $scope.labels = data.labels;
       $scope.series = data.series;
-      $scope.data = [data.payments];
+      $scope.data = [data.payments, data.alternativePayments];
 
-      $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
+      $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }, { yAxisID: 'y-axis-2'}];
       $scope.options = {
         responsive: true,
         maintainAspectRatio: false,
@@ -133,11 +136,16 @@ angular.module('Widget', ['chart.js', 'ng', 'ngMaterial', 'ngAnimate', 'ngAria']
                 display: true,
                 labelString: 'How Much You Owe'
               }
+            },
+            {
+              id: 'y-axis-2',
+              type: 'linear',
+              display: true,
+              position: 'left'
             }
           ]
         }
       };
     })
   }
-
 })
--- f-end/server/server.js
+++ f-end/server/server.js
@@ -12,19 +12,14 @@ app.use(bodyParser.json());
 
 const studentLoanRoute = require('./routes')
 
-app.use('/', studentLoanRoute)
-
-app.post('/api/loaninfo', (req, res) => {
-  res.send(req.body);
-})
-
+app.use('/', studentLoanRoute);
 
 //set port
 const port = process.env.PORT || 5000;
 
 app.listen(port, (err) => {
   if(err) {
-    console.log('Error occured : ', err);
+    console.log('Error occurred : ', err);
   }
   console.log('Server is listening to port : ', port);
 });
