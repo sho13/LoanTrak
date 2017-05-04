fc55ad1f88cf8c78038d828714c35f129976d20a
Sean Ho <seansinflipflops@gmail.com>
2017-05-04 14:53:33 -0400

    style

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var concatCss = require('gulp-concat-css');
 var uglifycss = require('gulp-uglifycss');
 
 gulp.task('sass', function() {
-  return gulp.src('./f-end/client/stylesheets/*.sass')
+  return gulp.src('./src/client/stylesheets/*.sass')
 // looks for sass files in public_dev/stylesheets and its subdirectories
     .pipe(sass().on('error', sass.logError)) // shows errors if there are any
     .pipe(autoprefixer({ // autoprefixer
@@ -16,11 +16,11 @@ gulp.task('sass', function() {
     .pipe(uglifycss({ // uglifies and saves space
       "max-line-len": 80
     }))
-    .pipe(gulp.dest('./f-end/client/stylesheets/')); // output file directory
+    .pipe(gulp.dest('./src/client/stylesheets/')); // output file directory
 });
 
 gulp.task('watch', function() {
-  gulp.watch('./f-end/client/stylesheets/*.scss', ['sass']);
+  gulp.watch('./src/client/stylesheets/*.sass', ['sass']);
 // if you change any of the files under the public_dev/stylesheets directory
 // and its subdirectories, it'll run gulp automatically.
 });
